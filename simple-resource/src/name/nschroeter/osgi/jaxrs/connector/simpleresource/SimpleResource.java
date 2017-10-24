a7f4f4f540ec3a468fa74f9cfa782cf5b5debbc3
nschroeter <me@nschroeter.name>
2017-10-24 22:30:59 +0200

    Simple example of annotatin JAX-RS resource interfaces

--- simple-resource/src/name/nschroeter/osgi/jaxrs/connector/simpleresource/SimpleResource.java
+++ simple-resource/src/name/nschroeter/osgi/jaxrs/connector/simpleresource/SimpleResource.java
@@ -5,10 +5,10 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/")
+@Path("/hello")
 public interface SimpleResource {
 
 	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	HelloTO helloWorld();
+	@Produces(MediaType.TEXT_PLAIN)
+	String hello();
 }
--- simple-resource/src/name/nschroeter/osgi/jaxrs/connector/simpleresource/provider/SimpleResourceProvider.java
+++ simple-resource/src/name/nschroeter/osgi/jaxrs/connector/simpleresource/provider/SimpleResourceProvider.java
@@ -3,14 +3,13 @@ package name.nschroeter.osgi.jaxrs.connector.simpleresource.provider;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.ServiceScope;
 
-import name.nschroeter.osgi.jaxrs.connector.simpleresource.HelloTO;
 import name.nschroeter.osgi.jaxrs.connector.simpleresource.SimpleResource;
 
 @Component(scope = ServiceScope.PROTOTYPE)
 public class SimpleResourceProvider implements SimpleResource {
 
 	@Override
-	public HelloTO helloWorld() {
-		return new HelloTO();
+	public String hello() {
+		return "Hello World!";
 	}
 }
